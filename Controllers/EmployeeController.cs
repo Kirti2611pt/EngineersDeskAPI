737c819ab626c3ad7e028a7cebe1e6ad5984217e
Kirti Prakash Tishu <talktokirtiprakash@gmail.com>
2022-11-22 23:40:22 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using DatabaseProject.Interface;
+using DatabaseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngineersDeskAPI.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _EmployeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _EmployeeRepository = employeeRepository;
+        }
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            try
+            {
+                var employees = _EmployeeRepository.GetEmployees();
+                return Ok(employees);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetEmployeeById(int Id)
+        {
+            try
+            {
+                var employees = _EmployeeRepository.GetEmployeeById(Id);
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            try
+            {
+                var addedemployees = _EmployeeRepository.AddEmployee(employee);
+                return Ok(addedemployees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+    }
+}
